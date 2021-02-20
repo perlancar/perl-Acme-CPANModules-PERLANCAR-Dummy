0e327b2f8b09462dbe8a52705e327fea2976afe3
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-20 10:40:22 +0700

    Release v0.001

--- lib/Acme/CPANModules/PERLANCAR/Dummy.pm
+++ lib/Acme/CPANModules/PERLANCAR/Dummy.pm
@@ -7,22 +7,32 @@ package Acme::CPANModules::PERLANCAR::Dummy;
 
 our $LIST = {
     summary => 'A dummy Acme::CPANModules list for testing',
+    entry_features => {
+        foo => {summary=>'Foo feature (bool)'},
+        bar => {summary=>'Bar feature (bool)'},
+        baz => {summary=>'Baz feature (string)', schema=>'str*'},
+    },
     entries => [
         {
             module => "App::Trrr",
-            features => {foo=>{value=>undef, summary=>'Some note'}, bar=>{value=>undef, summary=>'Some note'}},
+            features => {
+                foo => {value=>undef, summary=>'Some note'},
+                bar => {value=>undef, summary=>'Some note'},
+                baz => 'value1',
+            },
         },
         {
             module => "App::Wax",
             features => {foo=>1, bar=>1}},
-        { module => "App::cpangrep", features=>{foo=>0, bar=>0} },
-        { module => "Acme::PPIx::MetaSyntactic", features=>{foo=>1, bar=>0} },
+        {
+            module => "App::cpangrep",
+            features => {foo=>0, bar=>0},
+        },
+        {
+            module => "Acme::PPIx::MetaSyntactic",
+            features => {foo=>1, bar=>0},
+        },
     ],
-    summary => "Modules I plus while MetaCPAN's ++ is not working",
-    entry_features => {
-        foo => {summary=>'Foo feature'},
-        bar => {summary=>'Bar feature'},
-    },
 };
 
 1;
